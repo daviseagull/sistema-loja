09314bc9b3e7baeaaebdf7b08daeea5424625d6e
Davi Seagull <davi.seagull@gmail.com>
2021-10-21 20:56:38 -0300

    refactor: adição da documentação ao ItemController

--- src/main/java/br/com/roseai/sistemaloja/controller/ItemController.java
+++ src/main/java/br/com/roseai/sistemaloja/controller/ItemController.java
@@ -1,4 +1,92 @@
 package br.com.roseai.sistemaloja.controller;
 
+import br.com.roseai.sistemaloja.entity.Item;
+import br.com.roseai.sistemaloja.model.ItemDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface ItemController {
+
+    @Operation (
+            summary = "Endpoint para retorno de um item.",
+            responses = {
+                    @ApiResponse (
+                            description = "Dados obtidos com sucesso.",
+                            responseCode = "200",
+                            content = @Content (
+                                    mediaType = "application/json",
+                                    schema = @Schema (implementation = Item.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<Item> getItem(@PathVariable String id);
+
+    @Operation (
+            summary = "Endpoint para retorno de todos itens.",
+            responses = {
+                    @ApiResponse (
+                            description = "Dados obtidos com sucesso.",
+                            responseCode = "200",
+                            content = @Content (
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema (implementation = Item.class))
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<List<Item>> getItens();
+
+    @Operation (
+            summary = "Endpoint para criação de um item.",
+            responses = {
+                    @ApiResponse (
+                            description = "Item criado com sucesso.",
+                            responseCode = "200",
+                            content = @Content (
+                                    mediaType = "application/json",
+                                    schema = @Schema (implementation = String.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<String> createItem(@RequestBody ItemDTO item);
+
+    @Operation (
+            summary = "Endpoint para atualização de um item.",
+            responses = {
+                    @ApiResponse (
+                            description = "Item atualizado com sucesso.",
+                            responseCode = "200",
+                            content = @Content (
+                                    mediaType = "application/json",
+                                    schema = @Schema (implementation = String.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<String> updateItem(@PathVariable String id, @RequestBody ItemDTO item);
+
+    @Operation (
+            summary = "Endpoint para exclusão de um item.",
+            responses = {
+                    @ApiResponse (
+                            description = "Item excluido com sucesso.",
+                            responseCode = "200",
+                            content = @Content (
+                                    mediaType = "application/json",
+                                    schema = @Schema (implementation = String.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<String> deleteItem(@PathVariable String id);
 }
--- src/main/java/br/com/roseai/sistemaloja/model/ItemDTO.java
+++ src/main/java/br/com/roseai/sistemaloja/model/ItemDTO.java
@@ -1,18 +1,45 @@
 package br.com.roseai.sistemaloja.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Builder
 public class ItemDTO {
 
+    @Id
+    @Schema(
+            name = "codigo",
+            description = "Codigo do item.",
+            example = "875ABF4543",
+            required = true
+    )
     private String codigo;
 
+    @Schema (
+            name = "nome",
+            description = "Nome do item.",
+            example = "Arroz 5KG",
+            required = true
+    )
     private String nome;
 
+    @Schema (
+            name = "preço",
+            description = "Preço unitário do item.",
+            example = "21.40",
+            required = true
+    )
     private Double preco;
 
+    @Schema (
+            name = "quantidade",
+            description = "Quantidade do item no estoque.",
+            example = "100",
+            required = true
+    )
     private Integer quantidade;
 
 }
