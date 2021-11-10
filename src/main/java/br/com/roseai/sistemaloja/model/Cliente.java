a42e5e26e43d99e80f90c66b649643efd17a2467
Davi Seagull <davi.seagull@gmail.com>
2021-11-10 10:39:50 -0300

    refactor: adicionando documentação para o swagger

--- src/main/java/br/com/roseai/sistemaloja/model/Cliente.java
+++ src/main/java/br/com/roseai/sistemaloja/model/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.roseai.sistemaloja.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,8 +9,18 @@ import lombok.*;
 @EqualsAndHashCode
 public class Cliente {
 
+    @Schema(
+            name = "cpf",
+            description = "CPF do cliente que realizou a venda.",
+            required = true
+    )
     private String cpf;
 
+    @Schema(
+            name = "nome",
+            description = "Nome do cliente que realizou a venda.",
+            required = true
+    )
     private String nome;
 
 }
--- src/main/java/br/com/roseai/sistemaloja/model/ResumoVendaDTO.java
+++ src/main/java/br/com/roseai/sistemaloja/model/ResumoVendaDTO.java
@@ -1,5 +1,6 @@
 package br.com.roseai.sistemaloja.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,14 +9,42 @@ import lombok.*;
 @EqualsAndHashCode
 public class ResumoVendaDTO {
 
+    @Schema(
+            name = "id",
+            description = "Código único da venda.",
+            example = "543234",
+            required = true
+    )
     private String id;
 
+    @Schema(
+            name = "nome",
+            description = "Nome do cliente que realizou a venda.",
+            required = true
+    )
     private String nome;
 
+    @Schema(
+            name = "cpf",
+            description = "CPF do cliente que realizou a venda.",
+            required = true
+    )
     private String cpf;
 
+    @Schema(
+            name = "valorTotal",
+            description = "Valor total da venda.",
+            example = "210.53",
+            required = true
+    )
     private Double valorTotal;
 
+    @Schema(
+            name = "dataCriacao",
+            description = "Data de criação da venda.",
+            example = "20/10/2021",
+            required = true
+    )
     private String data;
 
 }
